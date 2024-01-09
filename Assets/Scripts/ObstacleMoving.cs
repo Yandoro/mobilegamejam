7a30d14aaf5e15bb28385026104831e4ec3c7360
Yandoro <kurogawaproducitons@gmail.com>
2024-01-09 12:04:38 +0100

    Obstacle delete?

--- Assets/Scripts/ObstacleMoving.cs
+++ Assets/Scripts/ObstacleMoving.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class ObstacleMoving : MonoBehaviour
 {
     public float speed = 5f;
+    public float destroyAfterSeconds = 1f;
     void Update()
     {
         Vector2 newPosition = transform.position;
         newPosition.x -= speed * Time.deltaTime; 
 
         transform.position = newPosition;
+
+        destroyAfterSeconds -= Time.deltaTime;
+        if ( destroyAfterSeconds <= 0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
