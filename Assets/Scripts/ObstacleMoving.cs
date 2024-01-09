9febbfaf64cdb9f18a934e1f77e38d31cb584aa3
Yandoro <kurogawaproducitons@gmail.com>
2024-01-09 14:00:08 +0100

    loose condition

--- Assets/Scripts/ObstacleMoving.cs
+++ Assets/Scripts/ObstacleMoving.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObstacleMoving : MonoBehaviour
 {
     public float speed = 5f;
     public float destroyAfterSeconds = 1f;
+    public float reloadTime = 4f;
+    private bool isDead = false;
     void Update()
     {
         Vector2 newPosition = transform.position;
-        newPosition.x -= speed * Time.deltaTime; 
+        newPosition.x -= speed * Time.deltaTime;
 
         transform.position = newPosition;
 
         destroyAfterSeconds -= Time.deltaTime;
         if ( destroyAfterSeconds <= 0f)
         {
-            Destroy(gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isDead = true;
+            Time.timeScale = 0f;
+            Debug.Log("Spiel pausiert!");
         }
     }
 }
